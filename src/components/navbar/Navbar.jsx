dce4d36cf838f4a9c2febef93eaf788bf08319c4
Parth Goel <146020919+ParthGoel01@users.noreply.github.com>
2024-06-28 13:50:51 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import './navbar.css';
 const Menu = () => {
   return (
     <>
-      <p><a href='#Home'>Home</a></p>
-      <p><a href='#wgpt3'>What is ChatBot?</a></p>
+      <p><a href='#home'>Home</a></p>
+      <p><a href='#what'>What is ChatBot?</a></p>
       <p><a href='#features'>Case Studies</a></p>
       <p><a href='#possibility'>Open AI</a></p>
       <p><a href='#blog'>Library</a></p>
